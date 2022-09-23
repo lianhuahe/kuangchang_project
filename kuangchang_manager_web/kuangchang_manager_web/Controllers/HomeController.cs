28e6c46de00bdb4418584028ec0c9cc7d2b8e529
LianHua <1270256109@qq.com>
2022-09-23 15:05:46 +0800

    no msg

---
+++ kuangchang_manager_web/kuangchang_manager_web/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace kuangchang_manager_web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ kuangchang_manager_web/kuangchang_manager_web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="kuangchang_manager_web.MvcApplication" Language="C#" %>
